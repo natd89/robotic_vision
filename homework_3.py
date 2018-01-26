877030ee51d9cf832ef83d6d5166cb12ddc3a1d6
nmd89 <nmd89@byu.edu>
2018-01-26 15:58:42 -0700

    added homework 3

---
+++ homework_3.py
@@ -0,0 +1,34 @@
+#!usr/bin/env python
+
+from Holodeck import Holodeck
+import numpy as np
+import sys
+import tty
+
+tty.setraw(sys.stdin.fileno())
+
+env = Holodeck.make('UrbanCity')
+
+def get_key():
+    return key = sys.stdin.read(3)
+    
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    
+
+
+
+
+
+
+
+
+
