eb01b892ec3b4a965a694ae12f96a41791eeb2af
nmd89 <nmd89@byu.edu>
2018-01-29 22:22:51 -0700

    homework 3 working with canny edge detection

--- homework_3.py
+++ homework_3.py
@@ -4,88 +4,75 @@ from Holodeck import Holodeck, Agents
 from Holodeck.Environments import HolodeckEnvironment
 from Holodeck.Sensors import Sensors
 from std_msgs.msg import Float64MultiArray
+import pygame
 import cv2
 import numpy as np
-import rospy
 
-class fly_uav():
-    
-    def __init__(self):
-        self.command = np.array([])
-        self.up    = None
-        self.down  = None
-        self.left  = None
-        self.right = None
-        rospy.Subscriber('uav_command', Float64MultiArray, self.get_key)
 
-    def get_key(self,msg):
-        self.command = self.command + msg.data
+def get_command(command,done):
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            # Figure out if it was an arrow key. If so
+                # adjust speed.
+            if event.key == pygame.K_r:
+                command = command + np.array([0,0,0,.5])
+            elif event.key == pygame.K_f:
+                command = command + np.array([0,0,0,-.5])
+            elif event.key == pygame.K_e:
+                command = command + np.array([0,0,.1,0])
+            elif event.key == pygame.K_d:
+                command = command + np.array([0,0,-.1,0])
+            elif event.key == pygame.K_w:
+                command = command + np.array([0,.1,0,0])
+            elif event.key == pygame.K_s:
+                command = command + np.array([0,-.1,0,0])
+            elif event.key == pygame.K_q:
+                command = command + np.array([.1,0,0,0])
+            elif event.key == pygame.K_a:
+                command = command + np.array([-.1,0,0,0])
+    return command
 
 
 if __name__=='__main__':
 
-    rospy.init_node('uav_test',anonymous=True)
-
     env = Holodeck.make("UrbanCity")
-
-    while not rospy.is_shutdown():
-
-        # Setup
-        pygame.init()
- 
-        # Set the width and height of the screen [width,height]
-        size = [700, 500]
-        screen = pygame.display.set_mode(size)
-        
-        pygame.display.set_caption("My Game")
-        
-        # Loop until the user clicks the close button.
-        done = False
- 
-        while not done:
-            # --- Event Processing
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    done = True
-                    # User pressed down on a key
- 
-                elif event.type == pygame.KEYDOWN:
-                    # Figure out if it was an arrow key. If so
-                    # adjust speed.
-                    if event.key == pygame.K_r:
-                        x_speed = -3
-                    elif event.key == pygame.K_v:
-                        x_speed = 3
-                    elif event.key == pygame.K_e:
-                        y_speed = -3
-                    elif event.key == pygame.K_c:
-                        y_speed = 3
-                    elif event.key == pygame.K_w:
-                        y_speed = -3
-                    elif event.key == pygame.K_x:
-                        y_speed = 3
-                    elif event.key == pygame.K_q:
-                        y_speed = -3
-                    elif event.key == pygame.K_z:
-                        y_speed = 3
- 
-                # User let up on a key
-                elif event.type == pygame.KEYUP:
-                    # If it is an arrow key, reset vector back to zero
-                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                        x_speed = 0
-                    elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                        y_speed = 0
     
-        state, reward, terminal, _ = env.step(self.command)
+    # Setup
+    pygame.init()
+    size = [200, 200]
+    screen = pygame.display.set_mode(size)
+
+    pygame.display.set_caption("My Game")
+
+    # Loop until the user clicks the close button.
+    done = False
+
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+ 
+    command = np.array([0,0,0,0])
+
+    # Hide the mouse cursor
+    pygame.mouse.set_visible(0)
+
+    while not done:
+        
+        command = get_command(command,done)
+
+        state, reward, terminal, _ = env.step(command)
         
         pixels = state[Sensors.PRIMARY_PLAYER_CAMERA]
         velocity = state[Sensors.VELOCITY_SENSOR]
 
+        gray_image = cv2.cvtColor(pixels, cv2.COLOR_BGR2GRAY)
+        edges = cv2.Canny(gray_image,50,300)
+        cv2.imshow('uav_filter',gray_image)
+        cv2.imshow('uav_canny',edges)
+        cv2.waitKey(1)
 
 
-
-
+    cv2.destroyAllWindows()
 
 
 
