32c9deceebf5151a56d849015ee9b26814a5d6e6
nmd89 <nmd89@byu.edu>
2018-02-12 15:06:08 -0700

    added height control to code

--- homework4/homework_4.py
+++ homework4/homework_4.py
@@ -89,14 +89,16 @@ if __name__=='__main__':
     mask = np.zeros_like(pixels_prev)
     
     flag = 1
-    h_c = 4
+    h_c = 2
     psi_c = .9
     psi = 0
     kp_psi = 2
     kp_theta = 0.01
+    kp_h = 3
     k = 0
     x_left = []
     x_right = []
+    x_bottom = []
     x_mid_left_y = []
     x_mid_right_y = []
     x_mid_left_x = []
@@ -154,29 +156,38 @@ if __name__=='__main__':
                     elif p0[i,0,0]>256 and p0[i,0,0]<256+128 and p0[i,0,1]>256-128 and p0[i,0,1]<256+128:
                         x_mid_right_y.append(p1[i,0,1] - p0[i,0,1])
                         x_mid_right_x.append(p1[i,0,0] - p0[i,0,0])
+                    elif p0[i,0,0]>256-128 and p0[i,0,0]<256+128 and p0[i,0,1]>256+128:
+                        x_bottom.append(p1[i,0,1] - p0[i,0,1])
+                    
 
                 sum_mid_x = np.sum(np.abs(x_mid_left_y)+np.abs(x_mid_right_y))
 
                 sum_x = np.sum(np.average(x_left)+np.average(x_right))
                 
                 if flag==3:
+                    # control roll
                     command[0] = kp_theta*sum_x 
-                    
+
+                    # control height
+                    command[3] = kp_h*np.average(np.abs(x_bottom))
+                    if (kp_h*np.average(np.abs(x_bottom))-h_c)>3:
+                        command[3] = h_c + 3
+
                     if command[0]>10*np.pi/180.:
                         command[0] = 10*np.pi/180.
                     if command[0]<-10*np.pi/180.:
                         command[0] = -10*np.pi/180.
 
                     if sum_mid_x > 80:
- 
+                        print('obstacle detected')
                         if (np.sum(np.abs(x_mid_left_y)) - np.sum(np.abs(x_mid_right_y))) > 20:
-                            command[2] = kp_psi*((psi_c-.3)-psi)
+                            command[2] = kp_psi*((psi_c-.4)-psi)
                             # command[1] = -0.1
                         elif (np.sum(np.abs(x_mid_left_y)) - np.sum(np.abs(x_mid_right_y))) < -20:
-                            command[2] = kp_psi*((psi_c+.3)-psi)
+                            command[2] = kp_psi*((psi_c+.4)-psi)
                             # command[1] = -0.1
                         elif (np.sum(np.abs(x_mid_left_y)) + np.sum(np.abs(x_mid_right_y)))>100 and (np.sum(np.abs(x_mid_left_x)) + np.sum(np.abs(x_mid_right_x)))>100:
-                            # print('collision immenent!')
+                            print('collision immenent!')
                             command[1] = 1.2
                             flag = 4
                             print('flag = ', flag)
@@ -184,7 +195,7 @@ if __name__=='__main__':
                             check = False
                     else:
                         command[2] = kp_psi*(psi_c-psi)
-                        command[1] = -0.2
+                        command[1] = -0.15
 
 
                 # Select good points
@@ -208,6 +219,7 @@ if __name__=='__main__':
         
             x_left = []
             x_right = []
+            x_bottom = []
             x_mid_left_y = []
             x_mid_right_y = []
             x_mid_left_x = []
@@ -215,18 +227,41 @@ if __name__=='__main__':
             k+=1
 
         if flag == 4:
+            # convert image to grayscale
+
+            gray_cur = cv2.cvtColor(pixels_cur, cv2.COLOR_BGRA2GRAY)                
+            p1, st, err = cv2.calcOpticalFlowPyrLK(gray_prev, gray_cur, p0, None, **lk_params)
+
+            # Select good points
+            good_new = p1[st==1]
+            good_old = p0[st==1]
+            
+            # draw the tracks
+            for i,(new,old) in enumerate(zip(good_new,good_old)):
+                a,b = new.ravel()
+                c,d = old.ravel()
+                mask = cv2.line(mask, (a,b),(c,d), [0,255,0], 1)
+                # frame = cv2.circle(pixels_cur,(a,b),2,[0,255,0],-1)
+            img = cv2.add(pixels_cur,mask)
+                
+            cv2.imshow('uav optical flow',img)
+            cv2.waitKey(25)
+                
+            mask = np.zeros_like(pixels_cur)
+            
+            gray_prev = deepcopy(gray_cur)
+
             if command[1] > 0:
                 if cnt%4==0:
-                    command[1] = 1 - cnt/300.0*1 
+                    command[1] = 1.2 - cnt/300.0*1.2 
                     state, reward, terminal, _ = env.step(command)
                 cnt += 1
                 if command[1]==0:
                     check = True
-                    print('check = ',check)
                     cnt = 0
             if check==True:
                 if cnt%4==0:
-                    command[1] = -.7 + cnt/100.0*.7
+                    command[1] = -.8 + cnt/100.0*.8
                     state, reward, terminal, _ = env.step(command)
                 cnt += 1
                 if command[1] == 0:
